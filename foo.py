78c4e291135d2286fd23b6d594d6fe642f3a7096
Will Ware <wware@alum.mit.edu>
2014-02-02 10:12:53 -0500

    rdflib understands rdfa in web pages

--- foo.py
+++ foo.py
@@ -5,3 +5,8 @@ g = rdflib.Graph()
 g.load('http://dbpedia.org/resource/Semantic_Web')
 
 pprint([(s, p, o) for s, p, o in g])
+
+# THIS WORKS, BUT GITHUB PUTS NOTHING USEFUL IN THIS
+# import rdflib
+# g = rdflib.Graph()
+# g.parse('https://github.com/wware/md-rdfa', format='rdfa')
